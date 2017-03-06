caf30beb30416dc8eeab84fa7a19010bd3ad967d
zhangzhifu <595373131@qq.com>
2017-03-06 19:52:42 +0800

    播放远程音乐

---
+++ 播放远程音频/播放远程音频/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  播放远程音频
+//
+//  Created by zhangzhifu on 2017/3/6.
+//  Copyright © 2017年 seemygo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 播放远程音频/播放远程音频/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  播放远程音频
+//
+//  Created by zhangzhifu on 2017/3/6.
+//  Copyright © 2017年 seemygo. All rights reserved.
+//
+ 
+#import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+
+@interface ViewController ()
+/** 播放器 */
+@property (nonatomic, strong) AVPlayer *player;
+@end
+
+@implementation ViewController
+
+#pragma mark - 懒加载
+- (AVPlayer *)player {
+    if (_player == nil) {
+        NSURL *url = [NSURL URLWithString:@"http://cc.stream.qqmusic.qq.com/C100003j8IiV1X8Oaw.m4a?fromtag=52"];
+//        _player = [AVPlayer playerWithURL:url];
+        
+        AVPlayerItem *item = [AVPlayerItem playerItemWithURL:url];
+        
+        _player = [AVPlayer playerWithPlayerItem:item];
+    }
+    return _player;
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [self.player play];iny
+}
+
+@end
